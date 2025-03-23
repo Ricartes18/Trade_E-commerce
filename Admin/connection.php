3be910f39be13aa7ccf72f820aa2d8c0d1a864c1
Cedric Luis <63840601+Ricartes18@users.noreply.github.com>
2025-03-23 13:05:59 +0800

    Delete Admin directory

--- Admin/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-// database connection
-    try{
-        $con = new mysqli("localhost", "root", "", "User");
-        $conp = new mysqli("localhost", "root", "", "merch_exchange");
-    } catch(mysqli_sql_exception) {
-        echo "Could not connect to database";
-    }
-?>
