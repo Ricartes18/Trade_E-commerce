648449b5d11ac58be88b54f2fec4999934476a4b
seowfie <144519653+seowfie@users.noreply.github.com>
2025-03-26 16:46:03 +0800

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,10 +1,10 @@
 <?php 
-    /* session_start();
+    session_start();
     include 'connection.php';
     if($_SESSION['role'] != 'admin'){
         header("Location: ../index.php");
         exit();
-    } */
+    }
 ?>
 
 <!DOCTYPE html>
@@ -12,7 +12,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/admin_dashboard.css">
+    <link rel="stylesheet" href="../css/dashboard_admin.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Changa+One:ital@0;1&family=Climate+Crisis&family=Dela+Gothic+One&display=swap" rel="stylesheet">
@@ -22,18 +22,21 @@
 <body>
     <div class="header">
         <div class="logo">
-            <img src="../img/PoCaSwap Logo.png" alt="Logo">
+        <a href="dashboard.php">
+            <img src="../images/PoCaSwap Logo.png" alt="Logo">
+        </a>
             <div class="title">
                 <span>PoCaSwap</span>
             </div>
         </div>
         <div class="nav">
-            <a href="#">Home</a>
-            <a href="#">Products</a>
-            <a href="#">Pending Orders</a>
-            <a href="#">Pending Trades</a>
+            <a href="dashboard.php">Home</a>
+            <a href="products.php">Products</a>
+            <a href="orders.php">Pending Orders</a>
+            <a href="pending_trades.php">Pending Trades</a>
         </div>
-    </div>
+        </div>
+
     
     <div class="main-container">
         <div class="welcome">
@@ -42,8 +45,8 @@
         </div>
         <div class="options">
             <a href="products.php" class="button view-modify">View and Modify Products</a>
-            <a href="#" class="button pending-orders">Pending Orders</a>
-            <a href="#" class="button pending-trade">Pending Trade</a>
+            <a href="orders.php" class="button pending-orders">Pending Orders</a>
+            <a href="pending_trades.php" class="button pending-trade">Pending Trade</a>
         </div>
     </div>
 </body>
