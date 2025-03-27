fcd2efb61d329ed1982c1c914589a3d7d0164cc4
Cedric Luis <63840601+Ricartes18@users.noreply.github.com>
2025-03-27 17:42:47 +0800

    Update order_tracker.php

--- order_tracker.php
+++ order_tracker.php
@@ -26,7 +26,7 @@ $tradesResult = mysqli_query($conp, $tradesQuery);
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, intial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Order & Trade Tracker</title>
     <link rel="stylesheet" href="css/footer.css">
     <link rel="stylesheet" href="css/header.css">
