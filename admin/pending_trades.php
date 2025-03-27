e47c92e2cd763fc360d0d1dd4d589ba7fc60a18b
Cedric Luis <63840601+Ricartes18@users.noreply.github.com>
2025-03-27 23:34:12 +0800

    Update pending_trades.php

--- admin/pending_trades.php
+++ admin/pending_trades.php
@@ -82,7 +82,7 @@
     <?php foreach($trade_statuses as $status) : ?>
         <div id="<?= htmlspecialchars($status, ENT_QUOTES) ?>" class="tab-content">
             <?php if(empty($trade_data[$status])) : ?>
-                <p>No <?=$trade_data[$status]?>Trades</p>
+                <p>No <?=$status?> Trades</p>
             <?php else: ?>
             <table>
                 <tr>
