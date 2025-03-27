1c185a0489ba7a5f092a23bd0e6ae15f1602810c
Cedric Luis <63840601+Ricartes18@users.noreply.github.com>
2025-03-28 01:19:13 +0800

    Update trade_upload.php

--- trade_upload.php
+++ trade_upload.php
@@ -64,20 +64,41 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 </head>
 <body>
     <!-- Navigation Bar -->
-        <nav class="navbar">
-            <ul class="nav-links">
-                <li><a href="index.php">Home</a></li>
-                <li><a href="shop.php">Shop</a></li>
-                <li><a href="order_tracker.php">Tracker</a></li>
-                <li><a href="redirection.php">Trades</a></li>
-            </ul>
-            <div class="logo">
-                <img src="images\PoCaSwap Logo.png" alt="Logo">
-            </div>
-            <div class="profile">
-                <a href="#"><img src="images\shopping_bag.png" alt="shopping bag"> @username</a>
-            </div>
-        </nav>
+        <header>
+            <nav class="navbar">
+                <ul class="nav-links">
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="shop.php">Shop</a></li>
+                    <li><a href="order_tracker.php">Tracker</a></li>
+                    <li><a href="redirection.php">Trades</a></li>
+                </ul>
+                <div class="logo">
+                    <a href="index.php"><img src="images/PoCaSwap Logo.png" alt="Logo"></a>
+                </div>
+                <div class="profile">
+                    <a href="cart.php"><img src="images/shopping_bag.png" alt="shopping bag"> 
+                        <?php 
+                            if(isset($_SESSION['role'])){
+                                if($_SESSION['role'] === "admin" ){
+                                    echo "<a href='admin/dashboard.php'>Dashboard</a>";
+                                } if(($_SESSION['role']) === "user"){
+                                echo "<p>".$_SESSION['username']."</p>";
+                                }
+                                echo "<div class='logout'>".
+                                    "<form action='logout.php' method='post'>". 
+                                        "<button class='logout-btn' type='submit' name='logout'>".
+                                            "<img src='images/logout_button.png' alt='Log out' class='logout-img'>".
+                                        "</button>
+                                    </form>
+                                </div>";
+                            } else {
+                                echo "<a href='login.php'>Login</a> <a>|</a> <a href='sign_up.php'>Sign Up</a>";
+                            }
+                        ?>
+                    </a>
+                </div>
+            </nav>
+        </header>
 
         <div class="trade-container">
             <!-- Trade Offer Section (Left) -->
